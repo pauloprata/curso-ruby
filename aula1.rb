8c59b1e6d90a0e137778737b8489e928701c955c
pauloprata <paulodanielprata@gmail.com>
2021-06-16 05:36:36 -0300

    o inicio

---
+++ aula1.rb
@@ -0,0 +1,22 @@
+puts 'Olá, qual é o seu nome?'
+name = gets.chomp
+print 'Digite sua idade '
+idade = gets.to_i
+print 'Digite o sexo '
+sexo = gets.chomp
+puts 'Seu nome é ' + name + '?  Que nome bonito!'
+puts 'Prazer em conhece-lo ' +name+ ''
+puts "Seu sexo é #{sexo}"
+
+if idade > 18 
+    print ' maior de idade '
+elsif idade < 18 
+    print ' Menor de idade ' 
+end
+puts "Sua idade é #{idade}"
+
+nome = "Paulo-Daniel-Prata".split("-")
+puts nome
+
+#length -1, pega sempre o ultimo item
+
