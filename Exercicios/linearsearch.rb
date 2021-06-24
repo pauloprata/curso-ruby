a3ed3f716355a19fb166d7409e5f366b713b24c9
pauloprata <paulodanielprata@gmail.com>
2021-06-24 07:34:21 -0300

    atualizando repo

--- Exercicios/linearsearch.rb
+++ Exercicios/linearsearch.rb
@@ -1,20 +1,23 @@
 def linearSearch (arr,n,x)
 (0..n).each do |i|
-    return _1 if arr[i] == x
+        if arr[i] == x
+        return i 
+        else
         return -1
         end
-    end
+    
     end
 end
 
  arr = [ 2, 3, 4, 10, 40 ];
- x = 10;
+ x = 40;
  n = arr.length;
 result = linearSearch(arr,n,x)
-if result == -1 
-    puts "Elements is not present in array"
-elsif
+result = arr.index(x)
+if result
     puts "Element present is index #{result}"
+elsif
+    puts "Elements is not present in array"
 end
 
              
