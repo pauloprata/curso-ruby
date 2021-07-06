5a9e923d302390de51f6027576635c10fcc4b580
pauloprata <paulodanielprata@gmail.com>
2021-07-06 07:17:12 -0300

    converter

--- Exercicios/summinute.rb
+++ Exercicios/summinute.rb
@@ -1,9 +1,7 @@
 def findTime(t, k)
 
-    minutes = ((Number(t[0]) - '0') * 10 +
-              Number(t[1]) - '0') * 60 +
-              ((Number(t[3]) - '0') * 10 +
-              Number(t[4]) - '0')
+    minutes = ((t[0] - '0') * 10 + t[1] - '0') * 60 +
+    ((t[3] - '0') * 10 + t[4] - '0');
 
     minutes += k
 
@@ -11,7 +9,7 @@ def findTime(t, k)
     min = minutes % 60
 
     if hour < 10
-        $stdout.print("0" +100+ " :")
+        $stdout.print("0" +hour+ " :")
     else 
         $stdout.print(hour.toFixed() )
     if min < 10 
