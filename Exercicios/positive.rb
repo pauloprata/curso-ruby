0f85d0bd806f914b8043a70baef0dd64ebf8e1d2
pauloprata <paulodanielprata@gmail.com>
2021-07-07 06:52:03 -0300

    atualizando repo

--- Exercicios/positive.rb
+++ Exercicios/positive.rb
@@ -1,33 +1,34 @@
 def rearrange(a,size)
   positive = 0
   negative = 1
-  
-  while true 
+  while true
     #varrendo o ponteiro até que o numero negativo não seja encotrado
     while positive < size &&  a[positive] >= 0
     positive +=2
     #varrendo até para não achar numeros positive
     while negative < size &&  a[negative] <=0
         negative +=2
-     if negative < size && positive < size 
-       temp =a[positive]   
+     if positive < size && negative < size 
+       temp = a[positive]   
        a[positive] = a[negative]
        a[negative] = temp
+  
      else 
       break
      end
-    end
+     end
   end
 end
 end
 
-arr =[ 1, -3, 5, 6, -3,6, 7, -4, 9, 10 ];
+arr =[ 1, -3, 5, 6, -3,6, 7, -4, 9, 10 ]
 n = arr.length
+rearrange(arr, n)
 
 (0..n).each do |i|
-   arr[i] 
+  $stdout.print( arr[i] ," ")
 end
-puts rearrange(arr, n);
+
 
 
 
