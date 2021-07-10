178d44b7613fa009a5b0b01a8a27ea85233d2e7b
pauloprata <paulodanielprata@gmail.com>
2021-07-10 09:35:34 -0300

    fatorial iterativo e recursivo

---
+++ Exercicios/factorial.rb
@@ -0,0 +1,19 @@
+#print " Digite um numero "
+#number = gets.chomp.to_i
+#fatorial = 1
+#for i in 1..number
+   # fatorial = fatorial * i
+#end
+#puts "O Fatorial de #{fatorial}"
+
+def factorialRec(n)
+    
+    if n==0 
+        return 1
+    elsif n==1
+         return 1 
+     else 
+        return n * factorialRec(n-1) 
+     end
+end
+puts factorialRec(30)
