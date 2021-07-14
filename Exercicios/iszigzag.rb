068201e79f3e6e0d3f9f0a97533da9103cbfa08f
pauloprata <paulodanielprata@gmail.com>
2021-07-14 06:02:08 -0300

    calculate age passo1 e passo2

---
+++ Exercicios/iszigzag.rb
@@ -0,0 +1,16 @@
+def isZigZag (number)
+      number.length - 2
+      a = b = c =
+     
+      if a < b > c or a > b < c
+       number[i] += 1
+    else 
+        return 0
+    number.each do |i|
+        number[i]
+    end
+end
+end
+
+number=[1,2,3,4,5]
+puts isZigZag(number)
---
+++ Exercicios/sum.rb
@@ -0,0 +1,5 @@
+numeros = [9, 13, 22]
+outros_numeros = [43, 52]
+
+todos_numeros = outros_numeros << numeros
+puts todos_numeros
